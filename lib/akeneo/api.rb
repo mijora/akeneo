f02ac849dfd88d40265535f374cf1b99c48ad9e2
Kestutis <kestutis@mijora.lt>
2024-05-28 11:37:48 +0300

    Add attribute options

--- lib/akeneo/api.rb
+++ lib/akeneo/api.rb
@@ -111,6 +111,10 @@ module Akeneo
       attribute_service.option(code, option_code)
     end
 
+    def attribute_options(code)
+      attribute_service.options(code)
+    end
+
     def create_or_update_attribute_option(attribute:, code:, options:)
       attribute_service.create_or_update_option(attribute, code, options)
     end
